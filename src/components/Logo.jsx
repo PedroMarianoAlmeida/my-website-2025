3f8d7049cbac423537467b74f23c499f41894610
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2025-03-05 04:11:55 -0300

    fix logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -7,16 +7,19 @@ export const Logo = () => {
       target="__blank"
       rel="noopener noreferrer"
     >
-      <div className="w-72 fixed top-4 left-4">
-        <FuzzyText
-          enableHover={false}
-          baseIntensity={1.5}
-          animationDuration={400}
-          idleDuration={2000}
-        >
-          {"< "}Pedro Programmer{" />"}
-        </FuzzyText>
+      <div className="w-full fixed top-0 md:top-4 left-1/2 -translate-x-1/2 flex md:justify-start justify-center items-center h-8">
+        <div className="w-72 h-8 bg-black flex justify-center items-center">
+          <FuzzyText
+            enableHover={false}
+            baseIntensity={1.5}
+            animationDuration={400}
+            idleDuration={2000}
+          >
+            {"< "}Pedro Programmer{" />"}
+          </FuzzyText>
+        </div>
       </div>
+      <div className="h-8 md:hidden block w-full " />
     </Link>
   );
 };
