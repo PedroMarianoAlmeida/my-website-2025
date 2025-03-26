d7bec57c20249f95ff92c0ff3a6c2ccb5c3d9d55
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2025-03-26 15:24:13 -0300

    add history section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import { HeroSection } from "@/components/hero-section/index";
 import { Stack } from "@/components/stack/index";
 import { Numbers } from "@/components/Numbers";
 import { Testimonials } from "@/components/Testimonials";
+import { History } from "@/components/history";
 
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center">
       <HeroSection />
       <Stack />
+      <History />
       <Numbers />
       <Testimonials />
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
